f1ab7c67a98a75e1b5524e38272a8e090ddca869
herreraorbezo-coder <herreraorbezo@gmail.com>
2026-01-08 14:51:16 -0500

    Update COT.py

--- COT.py
+++ COT.py
@@ -39,7 +39,8 @@ areas = {
     "MANTENIMIENTO": ["NILTON HINOSTROZA", "GUSTAVO VASQUEZ"],
     "E&IC": ["OMAR CAYLLAHUA", "MAURO BENAVENTE", "DAWI TORRES"],
     "ADMINISTRACION": ["ENRIQUE ESPINOZA", "LUCIO ZEVALLOS"],
-    "EHS": ["JOSE BENDEZU", "JACKER RUIZ", "MARCO ALVARADO"]
+    "EHS": ["JOSE BENDEZU", "JACKER RUIZ", "MARCO ALVARADO"],
+    "GIA": ["GARY NAVARRO", "JULIAN RODRIGUEZ", "ADDERLY DE LA CRUZ"]
 }
 
 menu = st.tabs(["📋 Registrar Actividad", "📊 Dashboard / KPIs"])
@@ -236,4 +237,5 @@ with menu[1]:
             st.warning(f"Cumplimiento promedio: {avg_cumplimiento}%")
         else:
             st.error(f"Cumplimiento promedio: {avg_cumplimiento}%")
+
 
